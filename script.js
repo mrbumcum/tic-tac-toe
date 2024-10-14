f4e6ecae9cf4003fb1cdda8554a4b64cbc7b0d0f
shucksow <102425362+shucksow@users.noreply.github.com>
2024-10-13 22:53:18 -0400

    feat: Chosen symbol functionality with welcome screen

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+const welcomeScreen = (function () {
+    const circle = document.querySelector('.circle');
+    const cross = document.querySelector('.cross');
+    const startButton = document.querySelector('.startButton');
+    const gameScreen = document.querySelector('.gameScreen');
+    const welcomeScreen = document.querySelector('.welcomeScreen');
+    let chosenSymbol = null;
+
+    const handleCircleClick = function () {
+        updateSelection(circle);
+        console.log('circle');
+    };
+
+    const handleCrossClick = function () {
+        updateSelection(cross);
+        console.log('cross');
+    };
+
+    const handleStartClick = function () {
+        welcomeScreen.classList.add('disabled');
+        gameScreen.classList.remove('disabled');
+    };
+
+    const updateSelection = function (selected) {
+        circle.classList.remove('selected');
+        cross.classList.remove('selected');
+        selected.classList.add('selected');
+        chosenSymbol = selected === circle ? 'circle' : 'cross';
+    }
+
+    circle.addEventListener('click', handleCircleClick);
+    cross.addEventListener('click', handleCrossClick);
+    startButton.addEventListener('click', handleStartClick);
+
+    return {
+        getChosenSymbol: function() {
+            return chosenSymbol;
+        }
+    }
+})();
+
 const gameBoard = (function () {
     const board = [
         ['', '', ''],
